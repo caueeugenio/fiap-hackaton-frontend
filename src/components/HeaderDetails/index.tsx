2ea3e233af5624525a018e11f761c59d0c6da158
Danilo Scorzoni Ré <danilo.scorzoni@gmail.com>
2025-01-24 14:09:43 -0300

    refactor: added a margin top

--- src/components/HeaderDetails/index.tsx
+++ src/components/HeaderDetails/index.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 export default function HeaderDetails({title}: Props) {
     return (
-        <h1 className='text-white text-4xl font-bold text-center pb-4'>
+        <h1 className='text-white text-4xl font-bold text-center pb-4 mt-8'>
           {title}
         </h1>
     )
