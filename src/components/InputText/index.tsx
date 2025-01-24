cae289fec3ced6b614d28b03682cd059163cf3a2
Danilo Scorzoni Ré <danilo.scorzoni@gmail.com>
2025-01-24 14:12:34 -0300

    fix: small change on component styling

--- src/components/InputText/index.tsx
+++ src/components/InputText/index.tsx
@@ -8,7 +8,7 @@ export default function InputText({fieldName, content, placeholder, value, onCha
     return (
         <div className="flex items-center justify-center">
             <label htmlFor={fieldName} className="text-white font-bold mr-3">{content}: </label>
-            <input name={fieldName} type="text" placeholder={placeholder} value={value} onChange={onChange} className="flex-1 px-2 py-2 rounded-sm" />
+            <input name={fieldName} type="text" placeholder={placeholder} value={value} onChange={onChange} className="flex-1 p-2 rounded-sm" />
         </div>
     )
 }
