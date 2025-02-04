cd8c58b0d4b5380bfb642d3abf2ede886c174869
Mayumi Matsuzoe <am.matsuzoe@hotmail.com>
2025-02-04 09:26:46 -0300

    feat: api endpoints

--- src/api/lib/axios.ts
+++ src/api/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const api: AxiosInstance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'http://localhost:3001',
   timeout: 1000 * 60 * 60 * 24, // 24 hours
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true,
---
+++ src/api/utils/messages.ts
@@ -0,0 +1,7 @@
+export const ERROR_MESSAGE = {
+  LOGIN_FAILED: 'Usuário ou senha inválidos',
+  UNAUTHORIZED: 'Faça o login para continuar',
+  INTERNAL_SERVER_ERROR: 'Erro interno do servidor, por favor tente novamente mais tarde',
+  NOT_FOUND: 'Not Found',
+  BAD_REQUEST: 'Bad Request',
+} 
